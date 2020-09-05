2bfad4ee33c5edafca2699464fb91f4568ddc48a
Kay <kahachang@gmail.com>
2020-09-06 00:53:31 +0800

    refactor: router

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+const routes = require('./routes')
 
 const usePassport = require('./config/passport')
-const passport = require('passport')
-const { create } = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
@@ -30,91 +25,7 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => console.error(error))
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-// 加入 middleware，驗證 reqest 登入狀態
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
-
-// new page
-app.get('/new', (req, res) => {
-  return res.render('new')
-})
-
-// create
-app.post('/', (req, res) => {
-  const UserId = req.user.id
-  const name = req.body.name
-  return Todo.create({ UserId, name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-
-})
-
-// detail page
-// toJSON 把資料轉換為 plain object 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-// edit page
-app.get('/:id/edit', (req, res) => {
-  const userId = req.user.id
-  const id = req.params.id
-
-  return Todo.findOne({ where: { id, userId } })
-    .then((todo) => res.render('edit', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
---
+++ routes/modules/todos.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+
+// new page
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// create
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ UserId, name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+
+})
+
+// read => detail page
+// toJSON 把資料轉換為 plain object 
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// update => edit page
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  console.log('userId:', userId, 'id:', id)
+
+  return Todo.findOne({ where: { id, userId } })
+    .then((todo) => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+
+
+module.exports = router
