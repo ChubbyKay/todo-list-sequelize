fefb3a5cf1349da8c5eff93623a332a3dc2a42df
Kay <kahachang@gmail.com>
2020-09-05 16:15:02 +0800

    upload todo-list-sequelize AC teach program

--- app.js
+++ app.js
@@ -1,19 +1,93 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+usePassport(app)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => console.error(error))
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+// 瀏覽
+// toJSON 把資料轉換為 plain object 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.listen(PORT, () => {
---
+++ migrations/20200904121849-add-userId-to-todo.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Todos', 'UserId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      reference: {
+        model: 'Users',
+        key: 'id'
+      }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Todos', 'UserId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
