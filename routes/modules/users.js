c62fe9fca7ecf9c3fbf81d332cfb071ee73fc170
Kay <kahachang@gmail.com>
2020-09-06 01:50:59 +0800

    feat: add authentication function, add error message in login and register and logout function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,15 +23,23 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都必須填寫' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '請再次確認密碼' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors, name, email, password, confirmPassword
+    })
+  }
+
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', { errors, name, email, password, confirmPassword })
     }
     return bcrypt
       .genSalt(10)
@@ -47,7 +55,9 @@ router.post('/register', (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  req.flash('success_msg', '你已經成功登出！')
+  res.redirect('/users/login')
 })
 
 module.exports = router
